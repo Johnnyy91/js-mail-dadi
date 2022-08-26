0d7bb9e816f31efbb1a4e5e1d18d2eee42588318
Johnnyy91 <claudiosardilli@gmail.com>
2022-08-26 18:13:32 +0200

    file creation

---
+++ js/script.js
@@ -0,0 +1,36 @@
+console.log('JS OK')
+
+let PCnumber = parseInt( Math.random() * 6 + 1);
+console.log(PCnumber)
+document.getElementById('PC').innerHTML = PCnumber + 'PC';
+
+let Usernumber = parseInt(Math.random() * 6 + 1);
+console.log(Usernumber)
+document.getElementById('User').innerHTML = Usernumber + 'User'
+
+const container = document.getElementById('container')
+
+const solution = document.createElement('div')
+container.append(solution)
+let result
+
+if (Usernumber > PCnumber)
+{
+    result = 'winner User'
+    solution.innerHTML = result
+    
+}
+else if(Usernumber === PCnumber)
+{
+    result = 'XXX'
+    solution.innerHTML = result
+    
+}
+else
+{
+    result = 'winner PC'
+    solution.innerHTML = result
+  
+}
+
+console.log(result)
